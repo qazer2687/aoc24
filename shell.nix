7c3e1cdc965c132e3d3041605f35447bbd4c2c93
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-05 13:56:50 +0000

    direnv add python3

--- shell.nix
+++ shell.nix
@@ -2,15 +2,26 @@
 pkgs.mkShell {
   # Packages
   packages = with pkgs; [
+    # C#
     (with dotnetCorePackages; combinePackages [
       sdk_6_0
       sdk_7_0
     ])
     csharpier
+
+    # Python
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      pandas
+      requests
+    ]))
   ];
 
-  # Environment
+  # C# Environment
   LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.dotnetCorePackages.sdk_7_0}/lib";
   PATH = "$PATH:/home/alex/.dotnet/tools";
   DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_7_0}";
+
+  # Python Environment
+  # ...
 }
